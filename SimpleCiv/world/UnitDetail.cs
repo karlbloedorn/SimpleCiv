48084359db3389c740b3f09bc7b6efa0fa28aa51
Karl Bloedorn <karl@bloedorn.us>
2015-12-19 13:38:03 -0600

    Fixed units.json

--- SimpleCiv/world/UnitDetail.cs
+++ SimpleCiv/world/UnitDetail.cs
@@ -20,9 +20,6 @@ namespace SimpleCiv.world
 
     public class UnitDetail
     {
-        public string objectFile;
-        public float scaleSize;
-        public bool singleUnit;
         public string attackSound;
         public string moveSound;
 
