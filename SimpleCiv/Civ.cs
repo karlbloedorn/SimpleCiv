1fae6829c1395ce45949e38ebe12621eabb95159
Peter Irish <coagentpai@gmail.com>
2015-12-19 13:01:44 -0600

    units: Add unit role

--- SimpleCiv/Civ.cs
+++ SimpleCiv/Civ.cs
@@ -175,20 +175,17 @@ namespace SimpleCiv
             var unitsConfig = FileLoader.LoadTextFile("assets/config/units.json");
             var unitTypeList = JsonConvert.DeserializeObject<List<UnitDetail>>(unitsConfig);
 
-            int unitLimit = 2;
+            int unitLimit = 0;
             int unitIndex = 0;
             foreach(var unit in unitTypeList)
             {
-                if(unitIndex > unitLimit)
+                if(unitLimit != 0 && unitIndex > unitLimit)
                 {
                     break;
                 }
-              var t = UnitType.None;
-              if( Enum.TryParse(unit.name, out t))
-                {
-                    unitTypes.Add(t, unit);
-                    unitIndex++;
-                }
+
+                unitTypes.Add(unit.name, unit);
+                unitIndex++;
             }                            
          
             var aa = 0;
--- SimpleCiv/world/UnitDetail.cs
+++ SimpleCiv/world/UnitDetail.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +8,41 @@ using System.Threading.Tasks;
 
 namespace SimpleCiv.world
 {
+    public enum UnitRole
+    {
+        LandRanged,
+        NavalRanged,
+        Air,
+        Siege,
+        Land,
+        Naval,
+    }
+
     public class UnitDetail
     {
-        public string objectFile;
-        public float scaleSize;
-        public bool singleUnit;
-        public string name;
+        [JsonConverter(typeof(StringEnumConverter)), JsonRequired]
+        public UnitRole role {
+            get; set;
+        }
+        [JsonRequired]
+        public string objectFile
+        {
+            get; set;
+        }
+        [JsonRequired]
+        public float scaleSize
+        {
+            get; set;
+        }
+        [JsonRequired]
+        public bool singleUnit
+        {
+            get; set;
+        }
+
+        [JsonConverter(typeof(StringEnumConverter)), JsonRequired]
+        public UnitType name {
+            get; set;
+        }
     }
 }
